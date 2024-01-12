48b30b3cc002f75492ea30837cff2cd9267bbad1
Tilochan <71354441+Tilochan@users.noreply.github.com>
2024-01-12 18:57:26 +0545

    modify-auth.js-file

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,12 +1,13 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import type { NextAuthOptions } from "next-auth";
+import type { Adapter } from "next-auth/adapters";
 import prisma from './db';
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import EmailProvider from "next-auth/providers/email";
 
 const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
